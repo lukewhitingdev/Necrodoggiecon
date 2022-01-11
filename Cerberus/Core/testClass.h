322d091dbb5b7ef2c4e4f100c242e2a17ded3fb4
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-01-11 17:07:53 +0000

    Added extra projects and initialy setup everything

---
+++ Cerberus/Core/testClass.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+class testClass
+{
+public:
+	static void test() 
+	{
+		printf("WOW THIS CAME FROM THE ENGINE! \n");
+	};
+};
+
---
+++ Necrodoggiecon/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Cerberus/Core/testClass.h"
+int main(int argc, char* argv[])
+{
+    printf("Hello World! \n");
+    testClass::test();
+    return 0;
+}
