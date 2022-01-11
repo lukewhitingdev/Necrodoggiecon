31b590e826eba6fffd180feefdd64c99a15c923b
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-01-11 17:19:25 +0000

    Added tools project

--- CerberusTools/main.cpp
+++ CerberusTools/main.cpp
@@ -2,7 +2,8 @@
 #include "Cerberus/Core/testClass.h"
 int main(int argc, char* argv[])
 {
-    printf("Hello World from GAME! \n");
+    printf("Hello World from TOOLS! \n");
     testClass::test();
+
     return 0;
 }
