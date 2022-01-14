977ab9c7754f03d4ea259a5acee89047b2b0779d
Ben Brown <49481217+Padingy@users.noreply.github.com>
2022-01-14 16:24:54 +0000

    Class files made

---
+++ Cerberus/Utility/CollisionManager/CollisionManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../../Engine.h"
+#include "../../CEntity.h"
+#include "Utility/DebugOutput/Debug.h"
+#include "../../CSpriteComponent.h"
+#include <stdio.h>
+
+struct CollisionManager
+{
+	static void Initialise();
+
+	
+};
+
