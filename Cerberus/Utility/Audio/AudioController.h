30810713b62f4021ce85d3d6fd49f7243992d694
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-01-24 14:01:35 +0000

    Added more functionality with ability to get emitters in range. Also edited loadAudio to return the audioCreated

--- Cerberus/Utility/Audio/AudioController.h
+++ Cerberus/Utility/Audio/AudioController.h
@@ -3,6 +3,7 @@
 #include "Dependencies/FMOD/api/core/inc/fmod_errors.h"
 #include "Utility/DebugOutput/Debug.h"
 #include "Utility/AssetManager/AssetManager.h"
+#include "Utility/Audio/CEmitter.h"
 #include "Vector3.h"
 
 class AudioController
@@ -11,14 +12,19 @@ public:
 	static void Initialize();
 	static void Shutdown();
 
-	static bool LoadAudio(std::string path, bool worldSpace);
+	static CAudio* LoadAudio(std::string path, bool worldSpace);
 	static bool PlayAudio(std::string path);
 	static bool StopAudio(std::string path);
 	static bool DestroyAudio(std::string path);
 
 	static void Update(Vector3 listenerPos, float deltaTime);
 
+	static std::vector<CEmitter*> getAllEntitiesWithinRange(Vector3 position);
+	static void addEmitter(CEmitter* emitter);
+	static void removeEmitter(CEmitter* emitter);
+
 private:
 	static FMOD::System* FMODSystem;
+	static std::vector<CEmitter*> emitters;
 };
 
