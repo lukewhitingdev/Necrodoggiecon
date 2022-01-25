9d8d3edd2b598611ce959cdd0775b222f9dd7294
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-01-25 15:59:58 +0000

    Removed Debug for Audio

--- Cerberus/CAICharacter.cpp
+++ Cerberus/CAICharacter.cpp
@@ -6,10 +6,6 @@ CAICharacter::CAICharacter()
 	viewSprite->LoadTexture("Resources\\viewFrustrum.dds");
 	viewSprite->SetTint(XMFLOAT4(0.0f, 0.0f, 0.0f, 0));
 	viewSprite->SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
-
-	audioEmitter = AddComponent<CAudioEmitterComponent>();
-	audioEmitter->Load("Resources/SmashMouth.mp3");
-	audioEmitter->Play();
 }
 
 void CAICharacter::Update(float deltaTime)
--- Cerberus/CAICharacter.h
+++ Cerberus/CAICharacter.h
@@ -2,7 +2,6 @@
 #include "Engine.h"
 #include "CEntity.h"
 #include "CSpriteComponent.h"
-#include "CAudioEmitterComponent.h"
 #include <stdio.h>
 
 class CAICharacter : public CEntity
@@ -10,7 +9,6 @@ class CAICharacter : public CEntity
 	float timeElapsed = 0;
 public:
 	class CSpriteComponent* viewSprite = nullptr;
-	CAudioEmitterComponent* audioEmitter = nullptr;
 	CAICharacter();
 	virtual void Update(float deltaTime) override;
 	virtual ~CAICharacter();
