4b451e6691abf9d55da640ee8fe873d85a0f9ad0
NasserKsous <55693958+NasserKsous@users.noreply.github.com>
2022-01-26 17:48:11 +0000

    removed the melee Ai for now

--- Cerberus/CAIMeleeCharacter.cpp
+++
@@ -1,8 +0,0 @@
-#include "CAIMeleeCharacter.h"
-
-void CAIMeleeCharacter::GetIntoCover(testCharacter* player)
-{
-	Vector3 vectorToPlayer = aiPosition - player->GetPosition();
-	Vector3 directionToPlayer = vectorToPlayer.Normalize();
-
-}
--- Cerberus/CAIMeleeCharacter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CAIController.h"
-class CAIMeleeCharacter :
-    public CAIController
-{
-    virtual void GetIntoCover(testCharacter* player) override;
-};
-
