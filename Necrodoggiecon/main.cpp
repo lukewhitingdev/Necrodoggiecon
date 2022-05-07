4f519933faed8fcd119437ce0db7f6beb4a9eeb1
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2022-05-07 21:34:32 +0100

    Added todo

--- Necrodoggiecon/main.cpp
+++ Necrodoggiecon/main.cpp
@@ -1,6 +1,16 @@
 //#include "Cerberus\Core\Engine.h"
 #include "Cerberus\Game\CPlayer.h"
 
+/*
+
+TODO:
+Move all Cerberus/Game code into this app
+Parse WndProc and WinMain in here and give it to the engine
+Make Engine::start or whatver to get the device working
+Re-create test bed using this application project
+
+*/
+
 int main() 
 {
 
