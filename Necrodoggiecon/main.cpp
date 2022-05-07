c5c2977f5cad4e3d645c6b8b9040043510bdd1e0
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2022-05-07 21:31:40 +0100

    Fixed Lib problems with FMOD and pulled over post-build dll step

--- Necrodoggiecon/main.cpp
+++ Necrodoggiecon/main.cpp
@@ -1,6 +1,12 @@
-#include "Cerberus\Core\Engine.h"
+//#include "Cerberus\Core\Engine.h"
+#include "Cerberus\Game\CPlayer.h"
 
 int main() 
 {
+
+	CPlayer* player = Engine::CreateEntity<CPlayer>();
+
+	player->visible = false;
+
 	return 0;
 }
