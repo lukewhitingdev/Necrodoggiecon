0871d5f817dbccf24a1f74effecd0a116698e13b
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2022-05-08 12:16:43 +0100

    Update main.cpp

--- Necrodoggiecon/main.cpp
+++ Necrodoggiecon/main.cpp
@@ -8,6 +8,7 @@ Move all Cerberus/Game code into this app
 Parse WndProc and WinMain in here and give it to the engine
 Make Engine::start or whatver to get the device working
 Re-create test bed using this application project
+Make pre-compiled lib files to speed up compilation
 
 */
 
