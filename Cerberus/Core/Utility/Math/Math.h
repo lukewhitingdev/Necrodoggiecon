2e74063644051fc2e038fe6ab5505a13f15e332b
Arrien Bidmead <37019464+ArrienBidmead@users.noreply.github.com>
2022-05-10 16:13:14 +0100

    Add screen to world space func
    Fixes issue #25 on github

--- Cerberus/Core/Utility/Math/Math.h
+++ Cerberus/Core/Utility/Math/Math.h
@@ -2,18 +2,36 @@
  * \file   Math.h
  * \brief  Utility Math Class
  * 
- * \author everyone
+ * \author Everyone
  * \date   May 2022
  *********************************************************************/
 
 #pragma once
 #include <random>
+#include "Cerberus/Core/Engine.h"
+#include "Cerberus/Core/Structs/CCamera.h"
 
+/**
+ * Class of all the static maths functions that don't fit into existing classes.
+ */
 class Math
 {
 public:
 	static int random(int min, int max)
 	{
 		return rand() % ((max - min) + 1) + min;
+	}
+
+	/**
+	 * Convert screen coords to world space.
+	 * Useful for converting the mouse to world space.
+	 * 
+	 * \param vec vector to be converted to world space.
+	 * \return 
+	 */
+	static XMFLOAT3 FromScreenToWorld(const XMFLOAT3& vec)
+	{
+		XMFLOAT3 newVec = XMFLOAT3((vec.x * 1 / Engine::camera.GetZoom() + Engine::camera.GetCameraPosition().x), (vec.y * 1 / Engine::camera.GetZoom() + Engine::camera.GetCameraPosition().y), vec.z);
+		return newVec;
 	}
 };
