8d81fcb6e2a6e8d19c458b4f14082d3b79014035
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2022-05-12 13:59:09 +0100

    Comments

--- Cerberus/Core/Utility/CameraManager/CameraManager.cpp
+++ Cerberus/Core/Utility/CameraManager/CameraManager.cpp
@@ -1,13 +1,32 @@
+/*****************************************************************//**
+ * \file   CameraManager.cpp
+ * \brief  Manages the cameras in the engine.
+ * 
+ * \author Luke Whiting
+ * \date   May 2022
+ *********************************************************************/
+
 #include "CameraManager.h"
 #include "Cerberus\Core\Utility\DebugOutput\Debug.h"
+
 std::map<std::uintptr_t,CCameraComponent*> CameraManager::cameras;
 std::uintptr_t CameraManager::renderingCamera;
 
+/**
+ * Adds a camera to the manager.
+ * 
+ * \param camera
+ */
 void CameraManager::AddCamera(CCameraComponent* camera)
 {
 	cameras.emplace(std::make_pair((uintptr_t)camera, camera));
 }
 
+/**
+ * Removes a camera from the manager. Further, if a rendering camera is delete it will move the rendering camera to the next camera in the manager.
+ * 
+ * \param camera
+ */
 void CameraManager::RemoveCamera(CCameraComponent* camera)
 {
 	if(cameras.find((uintptr_t)camera) != cameras.end())
@@ -34,6 +53,11 @@ void CameraManager::RemoveCamera(CCameraComponent* camera)
 	}
 }
 
+/**
+ * Returns the rendering camera.
+ * 
+ * \return 
+ */
 CCameraComponent* CameraManager::GetRenderingCamera()
 {
 	if(cameras.find(renderingCamera) != cameras.end())
@@ -45,6 +69,11 @@ CCameraComponent* CameraManager::GetRenderingCamera()
 	return nullptr;
 }
 
+/**
+ * Sets the rendering camera.
+ * 
+ * \param camera
+ */
 void CameraManager::SetRenderingCamera(CCameraComponent* camera)
 {
 	if(cameras.find((uintptr_t)camera) != cameras.end())
@@ -58,6 +87,11 @@ void CameraManager::SetRenderingCamera(CCameraComponent* camera)
 	}
 }
 
+/**
+ * Returns a vector of all cameras inside the manager.
+ * 
+ * \return 
+ */
 std::vector<CCameraComponent*> CameraManager::GetAllCameras()
 {
 	std::vector<CCameraComponent*> vec;
