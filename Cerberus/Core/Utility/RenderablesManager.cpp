e8bbd8d4633adf67873ee0b79ced04d8a034b5fb
GametraiN1 <37019464+ArrienBidmead@users.noreply.github.com>
2022-05-13 13:53:52 +0100

    more in progress stuff

--- Cerberus/Core/Utility/RenderablesManager.cpp
+++
@@ -1,1 +0,0 @@
-#include "RenderablesManager.h"
--- Necrodoggiecon/weaponUI.cpp
+++ Necrodoggiecon/weaponUI.cpp
@@ -5,26 +5,28 @@
 weaponUI::weaponUI()
 {
 	spriteBack = AddComponent<CSpriteComponent>();
-	spriteBack->LoadTextureWIC("Resources\\uiBackground.png");
-	spriteBack->SetRenderRect(XMUINT2(16, 16));
-	spriteBack->SetSpriteSize(XMUINT2(70, 70));
-	spriteBack->SetPosition(-600, -320, 1);
-	spriteBack->SetScale(1,1,0);
+	spriteBack->LoadTextureWIC("Resources\\blur1.png");
+	spriteBack->SetRenderRect(XMUINT2(64, 64));
+	spriteBack->SetSpriteSize(XMUINT2(64, 64));
+	spriteBack->SetPosition(-450, -280, 1);
+	spriteBack->SetScale(3,1,0);
 	spriteBack->SetAnchor(XMFLOAT2(0, 1));
 	spriteBack->translucency = true;
 	spriteBack->material->material.Material.translucent = true;
 	spriteBack->material->UpdateMaterial();
+	spriteBack->SetTint(XMFLOAT4(0, -1, -1, 0));
 
 	ammoBack = AddComponent<CSpriteComponent>();
-	ammoBack->LoadTextureWIC("Resources\\uiBackground.png");
-	ammoBack->SetRenderRect(XMUINT2(16, 16));
-	ammoBack->SetSpriteSize(XMUINT2(70, 70));
+	ammoBack->LoadTextureWIC("Resources\\blur3.png");
+	ammoBack->SetRenderRect(XMUINT2(64, 64));
+	ammoBack->SetSpriteSize(XMUINT2(64, 64));
 	ammoBack->SetPosition(-463, -320, 1);
 	ammoBack->SetScale(3, 1, 0);
 	ammoBack->SetAnchor(XMFLOAT2(0, 1));
 	ammoBack->translucency = true;
 	ammoBack->material->material.Material.translucent = true;
 	ammoBack->material->UpdateMaterial();
+	ammoBack->SetTint(XMFLOAT4(-1, 0, -1, 0));
 
 	textWeaponName = AddComponent<CTextRenderComponent>();
 	textWeaponName->SetJustification(TextJustification::Center);
