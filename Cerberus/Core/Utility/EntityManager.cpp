f4bd46b16f6fd18b5605a077e645fbd4d3b02e4f
Arrien Bidmead <37019464+ArrienBidmead@users.noreply.github.com>
2022-05-13 14:54:17 +0100

    Begin fleshing out entity manager

--- Cerberus/Core/Utility/EntityManager.cpp
+++ Cerberus/Core/Utility/EntityManager.cpp
@@ -1,1 +1,85 @@
 #include "EntityManager.h"
+#include "Core/CEntity.h"
+#include "Core/CComponent.h"
+#include <algorithm>
+
+std::unordered_map<uintptr_t, class CEntity*> EntityManager::entities = std::unordered_map<uintptr_t, class CEntity*>();
+std::unordered_map<uintptr_t, class CComponent*> EntityManager::opaqueComps = std::unordered_map<uintptr_t, class CComponent*>();
+std::vector<class CComponent*> EntityManager::translucentComps = std::vector<class CComponent*>();
+
+void EntityManager::AddEntity(CEntity* entityToAdd)
+{
+	entities.emplace(std::make_pair((uintptr_t)entityToAdd, entityToAdd));
+}
+
+void EntityManager::RemoveEntity(const CEntity* entityToRemove)
+{
+	if (entities.find((uintptr_t)entityToRemove) != entities.end())
+	{
+		entities.erase((uintptr_t)entityToRemove);
+	}
+	else
+	{
+		Debug::LogError("Tried to remove an entity that doesnt exist.");
+		return;
+	}
+}
+
+void EntityManager::AddComponent(CComponent* compToAdd)
+{
+	if(compToAdd->translucency)
+		translucentComps.push_back(compToAdd);
+	else
+		opaqueComps.emplace(std::make_pair((uintptr_t)compToAdd, compToAdd));
+}
+
+void EntityManager::RemoveComponent(const CComponent* compToRemove)
+{
+	if (compToRemove->translucency)
+	{
+		auto iterator = std::find(translucentComps.begin(), translucentComps.end(), compToRemove);
+
+		if (iterator != translucentComps.end())
+		{
+			Debug::LogError("Tried to remove an translucent component that doesnt exist.");
+			return;
+		}
+
+		translucentComps.erase(iterator);
+	}
+	else
+	{
+		if (opaqueComps.find((uintptr_t)compToRemove) != opaqueComps.end())
+		{
+			opaqueComps.erase((uintptr_t)compToRemove);
+		}
+		else
+		{
+			Debug::LogError("Tried to remove an opaque component that doesnt exist.");
+			return;
+		}
+	}
+}
+
+void EntityManager::SortTranslucentComponents()
+{
+	/*std::sort(translucentComps.begin(), translucentComps.end(), [](const CComponent& a, const CComponent& b) -> bool
+		{
+			return a. > b.mProperty;
+		});*/
+}
+
+const std::unordered_map<uintptr_t, class CEntity*>* EntityManager::GetEntitiesMap()
+{
+	return &entities;
+}
+
+const std::unordered_map<uintptr_t, class CComponent*>* EntityManager::GetOpaqueCompsMap()
+{
+	return &opaqueComps;
+}
+
+const std::vector<class CComponent*>* EntityManager::GetTranslucentCompsVector()
+{
+	return &translucentComps;
+}
--- Cerberus/Core/Utility/EntityManager.h
+++ Cerberus/Core/Utility/EntityManager.h
@@ -1,6 +1,24 @@
 #pragma once
+#include "Core/Engine.h"
+#include <unordered_map>
 
 class EntityManager
 {
+	static std::unordered_map<uintptr_t, class CEntity*> entities;
 
+	static std::unordered_map<uintptr_t, class CComponent*> opaqueComps;
+	static std::vector<class CComponent*> translucentComps;
+
+public:
+	static void AddEntity(class CEntity* entityToAdd);
+	static void RemoveEntity(const class CEntity* entityToRemove);
+
+	static void AddComponent(class CComponent* compToAdd);
+	static void RemoveComponent(const class CComponent* compToRemove);
+
+	static void SortTranslucentComponents();
+
+	static const std::unordered_map<uintptr_t, class CEntity*>* GetEntitiesMap();
+	static const std::unordered_map<uintptr_t, class CComponent*>* GetOpaqueCompsMap();
+	static const std::vector<class CComponent*>* GetTranslucentCompsVector();
 };
