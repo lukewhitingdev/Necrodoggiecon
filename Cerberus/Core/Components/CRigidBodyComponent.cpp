3992bc98c6a2fbaf9879e2029ee8a3cd381eab55
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-05-13 15:04:24 +0100

    Comments

--- Cerberus/Core/Components/CRigidBodyComponent.cpp
+++ Cerberus/Core/Components/CRigidBodyComponent.cpp
@@ -1,3 +1,11 @@
+/*****************************************************************//**
+ * \file   CRigidBodyComponent.cpp
+ * \brief  Adds basic rigid body physics to a entity.
+ * 
+ * \author Luke Whiting
+ * \date   Jan 2022
+ *********************************************************************/
+
 #include "CRigidBodyComponent.h"
 #include "Cerberus/Core/CEntity.h"
 
@@ -9,7 +17,13 @@ CRigidBodyComponent::~CRigidBodyComponent()
 {
 }
 
+
 static Vector3 pos;
+/**
+ * Updates the integration for the rigid body system.
+ * 
+ * \param deltaTime
+ */
 void CRigidBodyComponent::Update(float deltaTime)
 {
 	pos += velocity * deltaTime;							// Velocity Step
@@ -19,21 +33,41 @@ void CRigidBodyComponent::Update(float deltaTime)
 	velocity *= damping * deltaTime;
 }
 
+/**
+ * Sets the velocity of the rigidbody.
+ * 
+ * \param velo
+ */
 void CRigidBodyComponent::SetVelocity(const Vector3& velo)
 {
 	velocity = velo;
 }
 
+/**
+ * Returns the current velocity of the rigidbody.
+ * 
+ * \return 
+ */
 Vector3& CRigidBodyComponent::GetVelocity()
 {
 	return velocity;
 }
 
+/**
+ * Sets the acceleration of the rigidbody.
+ * 
+ * \param accel
+ */
 void CRigidBodyComponent::SetAcceleration(const Vector3& accel)
 {
 	acceleration = accel;
 }
 
+/**
+ * Returns the current acceleration of the rigidbody.
+ * 
+ * \return 
+ */
 Vector3& CRigidBodyComponent::GetAcceleration()
 {
 	return acceleration;
