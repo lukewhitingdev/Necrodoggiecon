c2891e9793e1245e924bee70141e1c3e9725c06a
Flynn Peter Brooks <57398331+FlynnPBrooks@users.noreply.github.com>
2022-05-20 10:30:10 +0100

    break to continue

--- Necrodoggiecon/Projectile.cpp
+++ Necrodoggiecon/Projectile.cpp
@@ -81,7 +81,7 @@ CEntity* Projectile::GetClosestEnemy(Vector3 actorPos)
 	{
 
 		if (actorPos.DistanceTo(enemy->GetPosition()) > 50)
-			break;
+			continue;
 
 		if (closestEnemy == nullptr)
 			closestEnemy = enemy;
@@ -109,7 +109,7 @@ CEntity* Projectile::GetClosestPlayer(Vector3 actorPos)
 	{
 
 		if (actorPos.DistanceTo(player->GetPosition()) > 50)
-			break;
+			continue;
 
 		if (closestPlayer == nullptr)
 			closestPlayer = player;
