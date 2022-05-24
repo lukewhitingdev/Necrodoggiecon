d574ee96ebe239a2b027a7b791626307f4eb925e
Flynn Peter Brooks <57398331+FlynnPBrooks@users.noreply.github.com>
2022-05-24 13:49:34 +0100

    added my doxy

--- Necrodoggiecon/Weapons/Ranged/MagicMissile.h
+++ Necrodoggiecon/Weapons/Ranged/MagicMissile.h
@@ -1,3 +1,11 @@
+/*****************************************************************//**
+ * \file   MagicMissile.h
+ * \brief  Header containing all the functions and variables needed for the Magic Missile.
+ *
+ * \author Flynn Brooks
+ * \date   May 2022
+ *********************************************************************/
+
 #pragma once
 #include <Necrodoggiecon/Weapons/RangeWeapon.h>
 #include <Necrodoggiecon/HomingProjectile.h>
