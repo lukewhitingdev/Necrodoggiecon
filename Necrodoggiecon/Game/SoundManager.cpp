7218f9680dc8256c3b11f2fa8b6c836585f4432a
Cathan Bertram <cathanbertram@gmail.com>
2022-05-26 20:48:29 +0100

    made sounds that arent weapon firing and bell ambient

--- Necrodoggiecon/Game/SoundManager.cpp
+++ Necrodoggiecon/Game/SoundManager.cpp
@@ -18,25 +18,25 @@ void SoundManager::Initialise()
 {
 	musicAudioEmitter = Engine::CreateEntity<AudioEmitterEntity>();
 
-	AddSound("Resources/Game/Audio/DeathSound.wav", "DeathSound", 1000.0f);
+	AddSound("Resources/Game/Audio/DeathSound.wav", "DeathSound", 1000.0f, true);
 	AddSound("Resources/Game/Audio/Footstep.wav", "StepSound", 100, true);
-	AddSound("Resources/Game/Audio/ShieldHit.wav", "ShieldHit", 1000.0f);
-	AddSound("Resources/Game/Audio/DeactivateInvis.wav", "DeactivateInvis", 1000.0f);
+	AddSound("Resources/Game/Audio/ShieldHit.wav", "ShieldHit", 1000.0f, true);
+	AddSound("Resources/Game/Audio/DeactivateInvis.wav", "DeactivateInvis", 1000.0f, true);
 	AddSound("Resources/Game/Audio/Bell.wav", "Bell", 1000.0f);
-	AddSound("Resources/Game/Audio/DogBark.wav", "DogBark", 1000.0f);
-	AddSound("Resources/Game/Audio/ItemPickup.wav", "ItemPickup", 10.0f);
-	AddSound("Resources/Game/Audio/DaggerStab.wav", "DaggerStab", 1000.0f);
+	AddSound("Resources/Game/Audio/DogBark.wav", "DogBark", 1000.0f, true);
+	AddSound("Resources/Game/Audio/ItemPickup.wav", "ItemPickup", 10.0f, true);
+	AddSound("Resources/Game/Audio/DaggerStab.wav", "DaggerStab", 1000.0f, true);
 	AddSound("Resources/Game/Audio/DaggerThrust.wav", "DaggerThrust", 1000.0f);
-	AddSound("Resources/Game/Audio/RapierHit.wav", "RapierHit", 1000.0f);
+	AddSound("Resources/Game/Audio/RapierHit.wav", "RapierHit", 1000.0f, true);
 	AddSound("Resources/Game/Audio/RapierThrust.wav", "RapierThrust", 1000.0f);
-	AddSound("Resources/Game/Audio/BroadswordHit.wav", "BroadswordHit", 1000.0f);
+	AddSound("Resources/Game/Audio/BroadswordHit.wav", "BroadswordHit", 1000.0f, true);
 	AddSound("Resources/Game/Audio/BroadswordSwing.wav", "BroadswordSwing", 1000.0f);
-	AddSound("Resources/Game/Audio/ArrowHit1.wav", "ArrowHit", 1000.0f);
+	AddSound("Resources/Game/Audio/ArrowHit1.wav", "ArrowHit", 1000.0f, true);
 	AddSound("Resources/Game/Audio/ShootBow.wav", "ShootBow", 1000.0f);
 	AddSound("Resources/Game/Audio/KnifeThrow.wav", "KnifeThrow", 1000.0f);
-	AddSound("Resources/Game/Audio/FireballKill.wav", "FireballHit", 1000.0f);
+	AddSound("Resources/Game/Audio/FireballKill.wav", "FireballHit", 1000.0f, true);
 	AddSound("Resources/Game/Audio/FireballShoot.wav", "FireballShoot", 1000.0f);
-	AddSound("Resources/Game/Audio/ScrollActivate.wav", "ScrollActivate", 1000.0f);
+	AddSound("Resources/Game/Audio/ScrollActivate.wav", "ScrollActivate", 1000.0f, true);
 	AddSound("Resources/Game/Audio/MagicMissileShoot.wav", "MagicMissileShoot", 1000.0f);
 	AddSound("Resources/Game/Audio/Click.wav", "UIClick", FLT_MAX, true);
 	AddSound("Resources/Game/Audio/ClearLevel.wav", "LevelClear", 1000.0f, true);
