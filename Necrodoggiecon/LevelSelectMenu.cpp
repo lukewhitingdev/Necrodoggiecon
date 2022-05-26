2ba137bffc6b21bb51eb5a30168772e39014eff1
Hononoha <56560087+Hononoha@users.noreply.github.com>
2022-05-26 13:25:00 +0100

    more small fixes

--- Necrodoggiecon/LevelSelectMenu.cpp
+++ Necrodoggiecon/LevelSelectMenu.cpp
@@ -28,72 +28,72 @@ void LevelSelectMenu::InitialiseCanvas()
 	CWidget_Image* Background = CreateImage(Vector2(0, 0), Vector2(.5, .5), -160);
 	Background->GetSprite()->LoadTextureWIC("Resources/Game/uiBackground.png");
 	Background->GetSprite()->SetRenderRect(XMUINT2(16, 16));
-	Background->GetSprite()->SetSpriteSize(XMUINT2(1500, 1000));
+	Background->GetSprite()->SetSpriteSize(XMUINT2(5000, 5000));
 	Background->GetSprite()->SetUseTranslucency(true);
 	Background->GetSprite()->SetTint(XMFLOAT4(-255, -255, -255, -0.1f));
 
 	std::string TitleText = "Level Select";
-	CWidget_Text* Title = CreateText(Vector2(0, 240), Vector2(1, 1), -165, TitleText);
+	CWidget_Text* Title = CreateText(Vector2(0, 240), Vector2(.5, .5), -165, TitleText);
 	Title->GetText()->SetFont("Resources/Engine/font.png");
 	Title->GetText()->SetText(TitleText);
 	Title->GetText()->SetScale(2, 2, 1);
 
 	std::string CloseButtonName = "Back";
-	CWidget_Button* CloseButton = CreateButton(Vector2(0, -258), Vector2(1, 1), CloseButtonName, -165);
+	CWidget_Button* CloseButton = CreateButton(Vector2(0, -258), Vector2(.5, .5), CloseButtonName, -165);
 	CloseButton->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::CloseMenu, this));
 	CloseButton->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	CloseButton->SetButtonSize(Vector2(256, 110));
 
 	std::string PlayButtonName = "Play";
-	CWidget_Button* PlayButton = CreateButton(Vector2(0, -128), Vector2(1, 1), PlayButtonName, -165);
+	CWidget_Button* PlayButton = CreateButton(Vector2(0, -128), Vector2(.5, .5), PlayButtonName, -165);
 	PlayButton->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::PlayLevel, this));
 	PlayButton->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	PlayButton->SetButtonSize(Vector2(256, 110));
 
 	std::string LVL0name = "Tutorial";
-	LVL0 = CreateButton(Vector2(0, 50), Vector2(1, 1), LVL0name, -165);
+	LVL0 = CreateButton(Vector2(0, 50), Vector2(.5, .5), LVL0name, -165);
 	LVL0->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevelTutorial, this));
 	LVL0->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL0->SetButtonSize(Vector2(256, 256));
 
 	std::string LVL1name = "Level 1";
-	LVL1 = CreateButton(Vector2(300, 50), Vector2(1, 1), LVL1name, -165);
+	LVL1 = CreateButton(Vector2(300, 50), Vector2(.5, .5), LVL1name, -165);
 	LVL1->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel1, this));
 	LVL1->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL1->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL2name = "Level 2";
-	LVL2 = CreateButton(Vector2(600, 50), Vector2(1, 1), LVL2name, -165);
+	LVL2 = CreateButton(Vector2(600, 50), Vector2(.5, .5), LVL2name, -165);
 	LVL2->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel2, this));
 	LVL2->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL2->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL3name = "Level 3";
-	LVL3 = CreateButton(Vector2(900, 50), Vector2(1, 1), LVL3name, -165);
+	LVL3 = CreateButton(Vector2(900, 50), Vector2(.5, .5), LVL3name, -165);
 	LVL3->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel3, this));
 	LVL3->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL3->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL4name = "Level 4";
-	LVL4 = CreateButton(Vector2(1200, 50), Vector2(1, 1), LVL4name, -165);
+	LVL4 = CreateButton(Vector2(1200, 50), Vector2(.5, .5), LVL4name, -165);
 	LVL4->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel4, this));
 	LVL4->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL4->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL5name = "Level 5";
-	LVL5 = CreateButton(Vector2(1500, 50), Vector2(1, 1), LVL5name, -165);
+	LVL5 = CreateButton(Vector2(1500, 50), Vector2(.5, .5), LVL5name, -165);
 	LVL5->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel5, this));
 	LVL5->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL5->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL6name = "Level 6";
-	LVL6 = CreateButton(Vector2(1800, 50), Vector2(1, 1), LVL6name, -165);
+	LVL6 = CreateButton(Vector2(1800, 50), Vector2(.5, .5), LVL6name, -165);
 	LVL6->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel6, this));
 	LVL6->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL6->SetButtonSize(Vector2(256, 256));
 	
 	std::string LVL7name = "Level 7";
-	LVL7 = CreateButton(Vector2(2100, 50), Vector2(1, 1), LVL7name, -165);
+	LVL7 = CreateButton(Vector2(2100, 50), Vector2(.5, .5), LVL7name, -165);
 	LVL7->Bind_OnButtonReleased(std::bind(&LevelSelectMenu::OpenLevel7, this));
 	LVL7->SetTexture("Resources/UI/UI_ButtonAtlas.dds");
 	LVL7->SetButtonSize(Vector2(256, 256));
