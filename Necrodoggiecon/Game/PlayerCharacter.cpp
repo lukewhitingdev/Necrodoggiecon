25884658c871bcfa73a0e13b816013fa7887182a
Ben Brown <49481217+Padingy@users.noreply.github.com>
2022-05-27 13:59:34 +0100

    Weapon balancing

--- Necrodoggiecon/Game/PlayerCharacter.cpp
+++ Necrodoggiecon/Game/PlayerCharacter.cpp
@@ -62,7 +62,7 @@ PlayerCharacter::PlayerCharacter()
 
 	weaponComponent = AddComponent<WeaponInterface>(NAME_OF(weaponComponent));
 	weaponComponent->SetUserType(USERTYPE::PLAYER);
-	weaponComponent->SetWeapon(new Rapier());
+	weaponComponent->SetWeapon(new Longsword());
 
 	weaponSprite = AddComponent<CSpriteComponent>(NAME_OF(weaponSprite));
 	UpdateWeaponSprite();
